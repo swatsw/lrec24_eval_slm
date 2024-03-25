390474433396d3bc3d5bcec0b38dd42418985597
audiopaperdemo <wsiyang19@gmail.com>
2024-03-25 12:02:41 +0100

    add wav

---
+++ wav/texts.js
@@ -0,0 +1,42 @@
+var context_texts = [
+[
+"A: Not bad.",
+"*B:  Say I've got two tickets for the concert this evening.Would you like to come with me ? ",
+],
+[
+"A: Wow! You must have been pretty far away from the city.",
+"*B: Yeah, my friend and I hiked in a very wild part of the national forest.",
+],
+[
+"A: Even if our company didn't have a dress code, I still think people would wear formal clothing to work.",
+"*B:  I wouldn't be so sure ... People want to wear what they feel most comfortable in . ",
+],
+[
+"A: How long will it take to reach the buffet?",
+"*B: Not long, because people rush for lunch.",
+],
+[
+"A: How to do it?",
+"*B: Just pick up a tray, then put a knife, a fork, a spoon and a paper napkin on it and stand in the line.",
+],
+[
+"A: Yes, I will schedule you for that time.",
+"*B: Thank you very much. See you then.",
+],
+[
+"A: He's fine. He retired last week. It's a turning point in his life. Now he can relax and enjoy his retirement.",
+"*B: He can spend more time with his grandchildren.",
+],
+[
+"A: Oh, I don't think he wants to. He wants to travel to several different countries around the world.",
+"*B: So, he wants to have a more active retirement. Good idea!",
+],
+[
+"A: How do you want to spend your old age?",
+"*B:  In the same way , probably . ",
+],
+[
+"A: Yes, is that you, Allen?",
+"*B: Yes, how are you doing?",
+],
+];
